3007f1322810ca0b54d4ef0330db3745109f3dc5
FelixZhang00 <l836828946@gmail.com>
2015-04-30 10:51:23 +0800

    init.

---
+++ app/src/androidTest/java/me/felixzhang/example/my_slidedrawer/ApplicationTest.java
@@ -0,0 +1,13 @@
+package me.felixzhang.example.my_slidedrawer;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application> {
+    public ApplicationTest() {
+        super(Application.class);
+    }
+}
