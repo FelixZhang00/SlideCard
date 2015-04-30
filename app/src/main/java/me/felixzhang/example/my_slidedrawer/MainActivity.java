b3ba5c867bc26c66945f40f71d525f96d6c52d30
FelixZhang00 <l836828946@gmail.com>
2015-04-30 14:23:52 +0800

    动画效果ok.

--- app/src/main/java/me/felixzhang/example/my_slidedrawer/MainActivity.java
+++ app/src/main/java/me/felixzhang/example/my_slidedrawer/MainActivity.java
@@ -2,8 +2,12 @@ package me.felixzhang.example.my_slidedrawer;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.BaseAdapter;
 
 import com.etsy.android.grid.StaggeredGridView;
+import com.nhaarman.listviewanimations.swinginadapters.AnimationAdapter;
+
+import me.felixzhang.example.my_slidedrawer.view.CardsAnimationAdapter;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +16,7 @@ public class MainActivity extends ActionBarActivity {
     private int[] imgIds = new int[]{R.drawable.image1, R.drawable.image2,
             R.drawable.image3, R.drawable.image4, R.drawable.image5,
             R.drawable.image6, R.drawable.image7};
+    private BaseAdapter mAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +24,12 @@ public class MainActivity extends ActionBarActivity {
         setContentView(R.layout.activity_main);
 
         mPage = (StaggeredGridView) findViewById(R.id.page);
-        mPage.setAdapter(new PageAdapter(this, imgIds));
+
+        mAdapter=new PageAdapter(this, imgIds);
+        AnimationAdapter animationAdapter = new CardsAnimationAdapter(mAdapter);
+        animationAdapter.setAbsListView(mPage);
+
+        mPage.setAdapter(animationAdapter);
 
     }
 
