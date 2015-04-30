0ce8b45306977875f07ca27a366f49cb5aece6fd
FelixZhang00 <l836828946@gmail.com>
2015-04-30 11:58:16 +0800

    静态显示ok.

--- app/src/main/java/me/felixzhang/example/my_slidedrawer/MainActivity.java
+++ app/src/main/java/me/felixzhang/example/my_slidedrawer/MainActivity.java
@@ -1,38 +1,27 @@
 package me.felixzhang.example.my_slidedrawer;
 
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+
+import com.etsy.android.grid.StaggeredGridView;
 
 
 public class MainActivity extends ActionBarActivity {
+
+    private StaggeredGridView mPage;
+    private int[] imgIds = new int[]{R.drawable.image1, R.drawable.image2,
+            R.drawable.image3, R.drawable.image4, R.drawable.image5,
+            R.drawable.image6, R.drawable.image7};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mPage = (StaggeredGridView) findViewById(R.id.page);
+        mPage.setAdapter(new PageAdapter(this, imgIds));
+
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
---
+++ app/src/main/java/me/felixzhang/example/my_slidedrawer/PageAdapter.java
@@ -0,0 +1,69 @@
+package me.felixzhang.example.my_slidedrawer;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+/**
+ * Created by felix on 15/4/30.
+ */
+public class PageAdapter extends BaseAdapter {
+
+    private Context mContext;
+    private int[] imgIds;
+
+    public PageAdapter(Context context, int[] imgIds) {
+        mContext = context;
+        this.imgIds = imgIds;
+    }
+
+    @Override
+    public int getCount() {
+        return imgIds.length;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = View.inflate(mContext, R.layout.card, null);
+        }
+        mViewHolder = getHolder(convertView);
+        mViewHolder.ivNormal.setImageResource(imgIds[position]);
+        return convertView;
+    }
+
+    private ViewHolder getHolder(View convertView) {
+        ViewHolder holder = (ViewHolder) convertView.getTag();
+        if (holder == null) {
+            holder = new ViewHolder(convertView);
+            convertView.setTag(holder);
+        }
+        return holder;
+    }
+
+    private ViewHolder mViewHolder;
+
+    static class ViewHolder {
+        TextView tvCaption;
+        ImageView ivNormal;
+
+        public ViewHolder(View convertView) {
+            tvCaption = (TextView) convertView.findViewById(R.id.tv_caption);
+            ivNormal = (ImageView) convertView.findViewById(R.id.iv_normal);
+        }
+    }
+
+}
